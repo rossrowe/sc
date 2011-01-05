aa7dae422176c46d30e310de7d89cfd2c300f327
Eric Allen <ericpallen@gmail.com>
2011-01-04 16:39:03 -0800

    bumping release

--- sauce_connect.py
+++ sauce_connect.py
@@ -37,7 +37,7 @@ except ImportError:
     import simplejson as json  # Python 2.5 dependency
 
 NAME = "sauce_connect"
-RELEASE = 23
+RELEASE = 24
 DISPLAY_VERSION = "%s release %s" % (NAME, RELEASE)
 PRODUCT_NAME = u"Sauce Connect"
 VERSIONS_URL = "http://saucelabs.com/versions.json"
