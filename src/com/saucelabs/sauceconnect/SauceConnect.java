d6076a34f53fa95c5bdd48b420d85d0826395de7
Eric Allen <ericpallen@gmail.com>
2011-01-05 05:53:10 -0800

    First integrated Connect + SSH + Proxy client

--- src/com/saucelabs/sauceconnect/SauceConnect.java
+++ src/com/saucelabs/sauceconnect/SauceConnect.java
@@ -1,22 +1,69 @@
 package com.saucelabs.sauceconnect;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.apache.commons.cli.*;
+import org.apache.commons.cli.Options;
 import org.python.core.*;
 import org.python.util.PythonInterpreter;
 
 public class SauceConnect {
 	public static PythonInterpreter interpreter;
 
-	/**
-	 * @param args
-	 */
+	private static CommandLine parseArgs(String[] args) throws ParseException{
+		Options options = new Options();
+		options.addOption("u", "username", true, "Sauce OnDemand account name");
+		options.addOption("k", "api-key", true, "Sauce OnDemand API key");
+		options.addOption("r", "rest-url", true, null);
+		CommandLineParser parser = new PosixParser();
+		return parser.parse(options, args);
+	}
+	
+	private static PyList generateArgsForSauceConnect(String username, String api_key, String domain, String rest_url){
+		ArrayList<PyString> args = new ArrayList<PyString>();
+		args.add(new PyString("-u"));
+		args.add(new PyString(username));
+		args.add(new PyString("-k"));
+		args.add(new PyString(api_key));
+		args.add(new PyString("-p"));
+		args.add(new PyString("4445"));
+		args.add(new PyString("-d"));
+		args.add(new PyString(domain));
+		args.add(new PyString("-s"));
+		args.add(new PyString("127.0.0.1"));
+		if(rest_url != null){
+			args.add(new PyString("--rest-url"));
+			args.add(new PyString(rest_url));
+		}
+		
+		return new PyList(args);
+	}
+	
 	public static void main(String[] args) {
 		interpreter = new PythonInterpreter();
 		interpreter.exec("import sauce_connect");
-		PyString[] pythonArgs = new PyString[args.length];
-		for(int index = 0; index < args.length; index++){
-			pythonArgs[index] = new PyString(args[index]);
+		CommandLine parsedArgs = null;
+		String domain = String.valueOf(new Random().nextInt(10000)) + "foo.proxy.saucelabs.com";
+		
+		try {
+			parsedArgs = parseArgs(args);
+		} catch (ParseException e1) {
+			System.err.println("Invalid command line: " + e1.getMessage());
+			return;
 		}
-		interpreter.set("arglist", new PyList(pythonArgs));
+		
+		SauceProxy proxy = new SauceProxy();
+		try {
+			proxy.start();
+		} catch (Exception e) {
+			log("Error starting proxy: "+e.getMessage());
+		}
+		
+		interpreter.set("arglist", generateArgsForSauceConnect(parsedArgs.getOptionValue('u'),
+				parsedArgs.getOptionValue('k'),
+				domain,
+				parsedArgs.getOptionValue("rest-url")));
 		interpreter.exec("from com.saucelabs.sauceconnect import ReverseSSH as JavaReverseSSH");
 		interpreter.exec("options = sauce_connect.get_options(arglist)");
 		interpreter.exec("sauce_connect.setup_logging(options.logfile, options.quiet)");
--- src/com/saucelabs/sauceconnect/SauceProxy.java
+++ src/com/saucelabs/sauceconnect/SauceProxy.java
@@ -5,15 +5,11 @@ import org.mortbay.http.SocketListener;
 import org.mortbay.jetty.Server;
 
 public class SauceProxy {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-        final SocketListener socketListener;
-
-        Server server = new Server();
-        socketListener = new SocketListener();
+	private Server server;
+	
+	public SauceProxy(){
+        server = new Server();
+        SocketListener socketListener = new SocketListener();
         socketListener.setMaxIdleTimeMs(60000);
         socketListener.setMaxThreads(512);
         socketListener.setPort(4445);
@@ -24,10 +20,17 @@ public class SauceProxy {
         root.setContextPath("/");
         ProxyHandler proxyHandler = new ProxyHandler(true, "", "", false, false);
 
-        // see docs for the lock object for information on this and why it is IMPORTANT!
         root.addHandler(proxyHandler);
         server.addContext(root);
-        server.start();
+	}
+	
+	public void start() throws Exception{
+		this.server.start();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SauceProxy proxy = new SauceProxy();
+        proxy.start();
 	}
 
 }
