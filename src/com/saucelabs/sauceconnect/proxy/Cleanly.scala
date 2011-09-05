5d07c9e97c9b7e442c3df5054088ba61dc13ff13
Steven Hazel <sah@saucelabs.com>
2011-09-04 17:04:38 -0700

    Less Java more Scala.  Cleanly my ass.

--- src/com/saucelabs/sauceconnect/proxy/Cleanly.scala
+++ src/com/saucelabs/sauceconnect/proxy/Cleanly.scala
@@ -16,9 +16,9 @@ limitations under the License.
 */
 /*
  * Adapted from the Selenium project under the Apache License 2.0
- * 
+ *
  * Portions Copyright 2011 Sauce Labs, Inc
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,63 +32,53 @@ limitations under the License.
  * limitations under the License.
  */
 
-package com.saucelabs.sauceconnect.proxy;
+package com.saucelabs.sauceconnect.proxy
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
-import java.nio.channels.Channel;
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.Reader
+import java.io.Writer
+import java.nio.channels.Channel
 
-public class Cleanly {
-  public static void close(Channel toClose) {
-    if (toClose == null) return;
-    
-    try {
-      toClose.close();
-    } catch (IOException e) {
-      // nothing that cna done. Ignoring.
-    }
-  }
-  
-  public static void close(InputStream toClose) {
-    if (toClose == null) return;
+object Cleanly {
+  def close(toClose: Channel) {
+    if (toClose == null) return
 
     try {
-      toClose.close();
-    } catch (IOException e) {
-      // nothing that can done. Ignoring.
-    }
+      toClose.close()
+    } catch { case e:IOException => }
   }
 
-  public static void close(OutputStream toClose) {
-    if (toClose == null) return;
+  def close(toClose: InputStream) {
+    if (toClose == null) return
 
     try {
-      toClose.close();
-    } catch (IOException e) {
-      // nothing that can done. Ignoring.
-    }
+      toClose.close()
+    } catch { case e:IOException => }
   }
 
-  public static void close(Reader reader) {
-    if (reader == null) return;
+  def close(toClose: OutputStream) {
+    if (toClose == null) return
 
     try {
-      reader.close();
-    } catch (IOException e) {
-      // nothing that can done. Ignoring.
-    }
+      toClose.close()
+    } catch { case e:IOException => }
   }
 
-  public static void close(Writer reader) {
-    if (reader == null) return;
+  def close(toClose: Reader) {
+    if (toClose == null) return
 
     try {
-      reader.close();
-    } catch (IOException e) {
-      // nothing that can done. Ignoring.
-    }
+      toClose.close()
+    } catch { case e:IOException => }
+  }
+
+  def close(toClose: Writer) {
+    if (toClose == null) return
+
+    try {
+      toClose.close()
+    } catch { case e:IOException => }
   }
 }
